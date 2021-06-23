6e92892f16f1500922bbe288ea5cd491f72a7bcb
Adetayo <akinsanyaadetayo7@gmail.com>
2021-06-23 14:54:36 +0100

    Implement sorting and filteering for the countries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,43 @@
-import Layout from '../component/Layout/Layout'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react';
 
-export default function Home() {
+
+import Layout from '../component/Layout/Layout';
+import styles from '../styles/Home.module.css';
+import SearchInput from '../component/SearchInput/SearchInput';
+import CountriesTable from '../component/CountriesTable/CountriesTable';
+
+
+export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("");
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  };
+
   return (
-    <Layout></Layout>
-      )
+    <Layout>
+      <div className={styles.counts}>found {filteredCountries.length} countries</div> 
+      <SearchInput placeholder="Filter by name, region or sub regions" onChange={handleInputChange} />
+      <CountriesTable countries={filteredCountries} />
+    </Layout>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
